514c9091305d103d134d1a778ca7c7f7eee81d7e
Lavya <lavya203@gmail.com>
2025-09-22 15:57:41 +0530

    Leraning tkinter

---
+++ hellotkinter.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Learning tkinterrr")  # window ka titile
+root.geometry("300x200")  
+
+
+label = tk.Label(root, text="Hello Tkinterr",font=("Arial",20))
+label.pack()
+
+def say_hello():
+    print("Button clicked!")
+
+button = tk.Button(root,text="click here",command=say_hello)
+button.pack()
+
+entry = tk.Entry(root, width=20)
+entry.pack()
+
+def show_input():
+    print("You typed:" , entry.get())
+
+button = tk.Button(root, text="Submit", command=show_input) 
+button.pack()  
+
+root. mainloop()  
